b0d1dc9d8b0ff60dc1e186fba03e121c28d6a5a6
ted <82011813+unnotedme@users.noreply.github.com>
2023-11-22 21:58:42 -0400

    typography (with tag error)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Card } from "./components/Card/Card";
 import { Styles } from "./components/GlobalStyles/Styles";
 import { Themes } from "./components/Themes/Themes";
+import { Header } from "./components/Header/Header";
+import { Typography } from "./components/Typography/Typography";
 
 function App() {
   return (
     <Themes>
       <Styles />
+        <Header>
+          <h1>Freelas</h1>
+        </Header>
       <Card>
-        <h1>Freelas</h1>
+        <Typography variable="h1">Freelas</Typography>
       </Card>
     </Themes>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Header = styled.header`
+    background: ${props => props.theme.colours.primary.a};
+    padding: ${props => props.theme.spacing.m};
+    `;
